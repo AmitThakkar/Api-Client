5a04f2cfd6cf54089fa52c41e059e5f8179063f5
Amit Thakkar <vigildbest@gmail.com>
2015-10-19 16:38:34 -0700

    SOme code clean up done.

--- client-code/testh-GET.js
+++ client-code/testh-GET.js
@@ -1,16 +1,18 @@
 /**
  * Created by Amit Thakkar on 10/19/15.
  */
-module.exports = {
-    "0": function (req, res, next) {
-        console.log("Hello");
-        next();
-    },
-    "1": function (req, res) {
-        getSchema("test", "user", function (schema) {
-            schema.find({}, (error, users) => {
-                res.status(200).json({users: users, isSuccess: false});
+((module) => {
+    module.exports = {
+        "0": function (req, res, next) {
+            console.log("Hello");
+            next();
+        },
+        "1": function (req, res) {
+            getSchema("test", "user", function (users) {
+                users.find({}, (error, users) => {
+                    res.status(200).json({users: users, isSuccess: false});
+                });
             });
-        });
-    }
-};
+        }
+    };
+})(module);
