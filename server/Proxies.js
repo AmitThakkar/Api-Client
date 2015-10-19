b650ca56a03f141c0b65d12b23f0b59bc99a0e67
Amit Thakkar <vigildbest@gmail.com>
2015-10-19 12:14:12 -0700

    Adding proxies file

---
+++ server/Proxies.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Amit Thakkar on 10/19/15.
+ */
+((global) => {
+    'use strict';
+    global.getSchema = (databaseName, tableName, callback) => {
+        callback(Proxy.create({
+            get: (obj, value) => {
+                return function () {
+                    logger.info(value, [].slice.call(arguments, 0, arguments.length - 1), arguments[arguments.length - 1]);
+                    return value
+                }
+            }
+        }));
+    };
+})(global);
